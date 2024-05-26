3c9473cf1d656456133bdf65610be6e8215e48f2
Artenlaclase <artenlaclase@gmail.com>
2024-05-26 01:25:27 -0400

    Contacto y formato

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
-const closeMenuBtn= document.querySelector(".close-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
 
 function toggleMenu() {
     menu.classList.toggle("menu_opened");
@@ -11,7 +11,7 @@ closeMenuBtn.addEventListener("click", toggleMenu);
 const menuLinks = document.querySelectorAll("  .menu a[href^=\"#\"]")
 
 menuLinks.forEach(menuLink => {
-    menuLink.addEventListener("click", function(){
+    menuLink.addEventListener("click", function () {
         menu.classList.remove("menu_opened");
     })
 
